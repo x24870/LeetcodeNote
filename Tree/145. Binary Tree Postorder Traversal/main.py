5074407583c8af1ad235a28ed4bcc845a0b3e923
pyk <x24870@gmail.com>
2020-05-08 20:37:23 +0800

    solved problem 145

---
+++ Tree/145. Binary Tree Postorder Traversal/main.py
@@ -0,0 +1,43 @@
+"""
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+"""
+class TreeNode():
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return str(self.val)
+
+root = TreeNode(1)
+root.right = TreeNode(2)
+root.right.left = TreeNode(3)
+
+
+""" 
+Solution format
+:type root: TreeNode
+:rtype: List[int]
+"""
+class RecursiveSolution(object):
+    def postorderTraversal(self, root):
+        traversalList = []
+        self.helper(root, traversalList)
+        return traversalList
+
+    def helper(self, root, lst):
+        if not root: return
+        self.helper(root.left, lst)
+        self.helper(root.right, lst)
+        lst.append(root.val)
+
+r = RecursiveSolution()
+print(r.postorderTraversal(root))
