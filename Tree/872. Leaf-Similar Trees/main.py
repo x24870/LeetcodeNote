039528b5ae8cbf5b3b2d6d2ce2be582973be2f0b
pyk <x24870@gmail.com>
2020-05-20 20:33:59 +0800

    resolved problem 872

---
+++ Tree/872. Leaf-Similar Trees/main.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# First Try
+class Solution:
+    def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
+        def get_leave_vals(node, lst):
+            if not node: return
+            if not node.left and not node.right:
+                lst.append(node.val)
+            get_leave_vals(node.left, lst)
+            get_leave_vals(node.right, lst)
+
+        root1_leave_vals = []
+        root2_leave_vals = []
+        get_leave_vals(root1, root1_leave_vals)
+        get_leave_vals(root2, root2_leave_vals)
+
+        return root1_leave_vals == root2_leave_vals
+
