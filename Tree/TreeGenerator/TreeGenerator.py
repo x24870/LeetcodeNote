7ff2a8212314e8f3eeba4a3943c1ec2340d64efb
pyk <x24870@gmail.com>
2020-05-22 20:44:41 +0800

    temp commit

--- Tree/TreeGenerator/TreeGenerator.py
+++ Tree/TreeGenerator/TreeGenerator.py
@@ -9,6 +9,23 @@ class TreeNode:
 
 class TreeGenerator:
     @classmethod
+    def remove_node_with_val_None(cls, root):
+        if not root: return
+        if root.left:
+            if root.left.val == None:
+                root.left = None
+            else:
+                cls.remove_node_with_val_None(root.left)
+
+        if root.right:
+            if root.right.val == None:
+                root.right = None
+            else:
+                cls.remove_node_with_val_None(root.right)
+
+        return root
+
+    @classmethod
     def get_bin_tree(cls, input_lst):
         lst = deepcopy(input_lst)
         if not lst:
@@ -35,7 +52,7 @@ class TreeGenerator:
             except IndexError:
                 print('Out of index, please check your list')
                     
-        return root
+        return cls.remove_node_with_val_None(root)
                 
     @classmethod
     def get_height(cls, root):
@@ -45,8 +62,8 @@ class TreeGenerator:
     @classmethod
     def get_filled_tree(cls, root, height):
         if height < 1: return
-        root.left = TreeNode()
-        root.right = TreeNode()
+        if not root.left: root.left = TreeNode(None)
+        if not root.right: root.right = TreeNode(None)
         cls.get_filled_tree(root.left, height-1)
         cls.get_filled_tree(root.right, height-1)
 
@@ -57,14 +74,17 @@ class TreeGenerator:
 
         # get height of tree
         total_layers = cls.get_height(root)
+        print('total_layers: ', total_layers)
 
-        # tree = deepcopy(root)
+        # copy root, and get filled tree
+        tree = deepcopy(root)
+        cls.get_filled_tree(tree, total_layers)
 
-        # cls.get_filled_tree(tree, total_layers)
         # start a queue for BFS
         queue = []
         # add root to queue
-        queue.insert(0, root)
+        # queue.insert(0, root)
+        queue.insert(0, tree)
 
         # index for 'generation' or 'layer' of tree
         gen = 0
@@ -97,75 +117,125 @@ class TreeGenerator:
                 if first_item_in_layer:
                     edges_string += " " * init_padding
 
+                if node != 'NONE':
+                    # construct edges layer
+                    #edge_sym = "/" if node.left and node.left.val is not " " else " "
+                    edge_sym = "/" if node.left else " "
+                    if first_item_in_layer:
+                        edges_string += " " * int(pow(2, total_layers - gen) - 1) + edge_sym
+                    else:
+                        edges_string += " " * int(pow(2, total_layers - gen + 1) + 1) + edge_sym
 
-                # construct edges layer
-                #edge_sym = "/" if node.left and node.left.val is not " " else " "
-                edge_sym = "/" if node.left else " "
-                if first_item_in_layer:
-                    edges_string += " " * int(pow(2, total_layers - gen) - 1) + edge_sym
+                    # edge_sym = "\\" if node.right and node.right.val is not " " else " "
+                    edge_sym = "\\" if node.right else " "
+                    edges_string += " " * (pow(2, total_layers - gen + 1) - 3) + edge_sym
+
+                    # conditions for dashes
+                    # if node.left and node.left.val == " ":
+                    if node.left:
+                        dash_left = "_"
+                    else:
+                        dash_left = "_"
+
+                    # if node.right and node.right.val == " ":
+                    if node.right:
+                        dash_right = "_"#" "
+                    else:
+                        dash_right = "_"
+
+                    # handle condition for extra spaces when node lengths don't match or are even:
+                    if extra_spaces_next_node:
+                        extra_spaces = 1
+                        extra_spaces_next_node = False
+                    else:
+                        extra_spaces = 0
+
+                    # account for longer data
+                    data_length = len(str(node.val))
+                    if data_length > 1:
+                        if data_length % 2 == 1: # odd
+                            if dash_count > 0:
+                                dash_count -= ((data_length - 1)/2)
+                            else:
+                                spaces_mid -= (data_length - 1)/2
+                                spaces_front -= (data_length - 1)/2
+                                if data_length is not 1: 
+                                    extra_spaces_next_node = True 
+                        else: # even
+                            if dash_count > 0:
+                                dash_count -= ((data_length)/2) - 1
+                                extra_spaces_next_node = True
+                                # dash_count += 1
+                            else:
+                                spaces_mid -= (data_length - 1)
+                                spaces_front -= (data_length - 1)
+
+                
+
+                    # print node with/without dashes
+                    if first_item_in_layer:
+                        print( ("*" * spaces_front) + (dash_left * dash_count) + str(node.val) + (dash_right * dash_count),  end='' )
+                        first_item_in_layer = False
+                    else:
+                        print( "+" * (spaces_mid-extra_spaces+1) + (dash_left * dash_count) + str(node.val) + (dash_right * dash_count), end='' )
+
+
+                    #if node.left: queue.enqueue(node.left)
+                    if node.left:
+                        queue.insert(0, node.left)
+                    else:
+                        queue.insert(0, 'NONE')
+                    #if node.right: queue.enqueue(node.right)
+                    if node.right:
+                        queue.insert(0, node.right)
+                    else:
+                        queue.insert(0, 'NONE')
                 else:
-                    edges_string += " " * int(pow(2, total_layers - gen + 1) + 1) + edge_sym
+                    edge_sym = " "
+                    if first_item_in_layer:
+                        edges_string += " " * int(pow(2, total_layers - gen) - 1) + edge_sym
+                    else:
+                        edges_string += " " * int(pow(2, total_layers - gen + 1) + 1) + edge_sym
 
-                # edge_sym = "\\" if node.right and node.right.val is not " " else " "
-                edge_sym = "\\" if node.right else " "
-                edges_string += " " * (pow(2, total_layers - gen + 1) - 3) + edge_sym
+                    edge_sym = " "
+                    edges_string += " " * (pow(2, total_layers - gen + 1) - 3) + edge_sym
 
-                # conditions for dashes
-                # if node.left and node.left.val == " ":
-                if node.left:
-                    dash_left = "_"#" "
-                else:
                     dash_left = "_"
-
-                # if node.right and node.right.val == " ":
-                if node.right:
-                    dash_right = "_"#" "
-                else:
                     dash_right = "_"
 
+                    # handle condition for extra spaces when node lengths don't match or are even:
+                    if extra_spaces_next_node:
+                        extra_spaces = 1
+                        extra_spaces_next_node = False
+                    else:
+                        extra_spaces = 0
 
-                # handle condition for extra spaces when node lengths don't match or are even:
-                if extra_spaces_next_node:
-                    extra_spaces = 1
-                    extra_spaces_next_node = False
-                else:
-                    extra_spaces = 0
+                    # account for longer data
+                    data_length = len(" ")
+                    if data_length > 1:
+                        if data_length % 2 == 1: # odd
+                            if dash_count > 0:
+                                dash_count -= ((data_length - 1)/2)
+                            else:
+                                spaces_mid -= (data_length - 1)/2
+                                spaces_front -= (data_length - 1)/2
+                                if data_length is not 1: 
+                                    extra_spaces_next_node = True 
+                        else: # even
+                            if dash_count > 0:
+                                dash_count -= ((data_length)/2) - 1
+                                extra_spaces_next_node = True
+                                # dash_count += 1
+                            else:
+                                spaces_mid -= (data_length - 1)
+                                spaces_front -= (data_length - 1)
+                    # print node with/without dashes
+                    if first_item_in_layer:
+                        print( ("A" * spaces_front) + (dash_left * dash_count) + " " + (dash_right * dash_count),  end='' )
+                        first_item_in_layer = False
+                    else:
+                        print( "B" * (spaces_mid-extra_spaces+1) + (dash_left * dash_count) + " " + (dash_right * dash_count), end='' )
 
-            
-                # account for longer data
-                data_length = len(str(node.val))
-                if data_length > 1:
-                    if data_length % 2 == 1: # odd
-                        if dash_count > 0:
-                            dash_count -= ((data_length - 1)/2)
-                        else:
-                            spaces_mid -= (data_length - 1)/2
-                            spaces_front -= (data_length - 1)/2
-                            if data_length is not 1: 
-                                extra_spaces_next_node = True 
-                    else: # even
-                        if dash_count > 0:
-                            dash_count -= ((data_length)/2) - 1
-                            extra_spaces_next_node = True
-                            # dash_count += 1
-                        else:
-                            spaces_mid -= (data_length - 1)
-                            spaces_front -= (data_length - 1)
-
-            
-
-                # print node with/without dashes
-                if first_item_in_layer:
-                    print( (" " * spaces_front) + (dash_left * dash_count) + str(node.val) + (dash_right * dash_count),  end='' )
-                    first_item_in_layer = False
-                else:
-                    print( " " * (spaces_mid-extra_spaces+1) + (dash_left * dash_count) + str(node.val) + (dash_right * dash_count), end='' )
-
-
-                #if node.left: queue.enqueue(node.left)
-                if node.left: queue.insert(0, node.left)
-                #if node.right: queue.enqueue(node.right)
-                if node.right: queue.insert(0, node.right)
 
             # print the fun squiggly lines
             if queue:
@@ -174,20 +244,27 @@ class TreeGenerator:
             # increase layer index
             gen += 1
 
+        print('\n')
 
 
-a = [1, 2, 3]
-print(a)
-root = TreeGenerator.get_bin_tree(a)
+
+# a = [1, 2, 3]
+# print(a)
+# root = TreeGenerator.get_bin_tree(a)
+# TreeGenerator.print_tree(root)
 
 
-a.append(2)
-a.append(None)
-a.append(None)
-a.append(1)
-print(a)
-root = TreeGenerator.get_bin_tree(a)
+# a.append(2)
+# a.append(None)
+# a.append(None)
+# a.append(1)
+# root = TreeGenerator.get_bin_tree(a)
+# TreeGenerator.print_tree(root)
 
-print('H: ', TreeGenerator.get_height(root))
+b = [1, None, 2, None, 3, None, 4]
+root = TreeGenerator.get_bin_tree(b)
+print(root.val)
+print(root.right.val)
+print(root.right.right.val)
+print(root.right.right.right.val)
 TreeGenerator.print_tree(root)
-print(root.left.right.right.left)
