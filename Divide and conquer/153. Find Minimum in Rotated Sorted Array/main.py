f8423be49e1456d9e1bb00dea86bd24c5f91d64f
pyk <x24870@gmail.com>
2020-05-24 23:09:22 +0800

    resolved problem 153

---
+++ Divide and conquer/153. Find Minimum in Rotated Sorted Array/main.py
@@ -0,0 +1,19 @@
+# First try
+class Solution:
+    def findMin(self, nums):
+        return min(nums)
+        
+
+class Solution:
+    def findMin(self, nums):
+        lo = 0
+        hi = len(nums) - 1
+
+        while lo < hi:
+            mid = (hi - lo)//2 + lo
+            if nums[mid] < nums[hi]:
+                hi = mid
+            else:
+                lo = mid + 1
+
+        return nums[lo]
