bdc3a17cdb54f3d51ca30013b2f963729d1fda19
pyk <x24870@gmail.com>
2020-06-14 20:07:42 +0800

    Add lower & upper bound solution to problem 98

--- BST/98. Validate Binary Search Tree/main.py
+++ BST/98. Validate Binary Search Tree/main.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 # first try
 # inorder traversal all nodes, then check if the list is ascending
@@ -11,17 +11,24 @@ class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         stack = []
         def dfs(node):
-            if not node: return node
-            if node.left:
-                dfs(node.left)
+            if not node: return
+            dfs(node.left)
             stack.append(node.val)
-            if node.right:
-                dfs(node.right)
+            dfs(node.right)
 
         dfs(root)
-        print(stack)
         for i in range(len(stack)-1):
             if stack[i] >= stack[i+1]:
                 return False
         return True
 
+# Define upper bound and lower bound
+from math import inf
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def helper(node, lower_bound, upper_bound):
+            if not node: return True
+            if not (lower_bound < node.val < upper_bound): return False
+            return helper(node.left, lower_bound, node.val) and helper(node.right, node.val, upper_bound)
+
+        return helper(root, -inf, inf)
