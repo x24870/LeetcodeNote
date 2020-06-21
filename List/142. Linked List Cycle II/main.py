1031a5ff7a31553897a17a1db2c399f992786ca9
pyk <x24870@gmail.com>
2020-06-21 19:26:36 +0800

    Add Floyd's cycle detection solution to 142

--- List/142. Linked List Cycle II/main.py
+++ List/142. Linked List Cycle II/main.py
@@ -19,4 +19,24 @@ class Solution:
         return None
 
 # Without using extra space
-# Floyd's algorithm
+# Reference: https://cs.stackexchange.com/questions/10360/floyds-cycle-detection-algorithm-determining-the-starting-point-of-cycle
+# Time complexity: O(n)
+# Space complexity: O(1)
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        fast = slow = head
+        while True:
+            fast = fast.next if fast else None
+            fast = fast.next if fast else None
+            slow = slow.next if slow else None
+            if fast == slow:
+                break
+
+        if fast == None: return None
+
+        # cycle exist
+        slow = head
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+        return fast
