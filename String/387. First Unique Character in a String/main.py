2491b9b96ca1f8fadbb5a5b1b52b51f6a9de622a
pyk <x24870@gmail.com>
2021-01-07 15:48:14 +0800

    add defaultdict solution to problem 387

--- String/387. First Unique Character in a String/main.py
+++ String/387. First Unique Character in a String/main.py
@@ -11,3 +11,39 @@ class Solution:
             if char_dict[s[i]] == 1: return i
         # not found
         return -1
+
+from collections import defaultdict
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        def return_zero():
+            return 0
+
+        char_dic = defaultdict(return_zero)
+
+        for c in s:
+            char_dic[c] += 1
+
+        char_lst = list(s)
+        for i in range(len(char_lst)):
+            if char_dic[char_lst[i]]: return i
+
+        return -1
+
+# use defaultdict
+# slightly faster
+from collections import defaultdict
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        def return_zero():
+            return 0
+
+        char_dic = defaultdict(return_zero)
+
+        for c in s:
+            char_dic[c] += 1
+
+        char_lst = list(s)
+        for i in range(len(char_lst)):
+            if char_dic[char_lst[i]] == 1: return i
+
+        return -1
