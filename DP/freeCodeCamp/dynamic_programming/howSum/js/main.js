53264f7b2673d72ed806ac1dd5d7eca328eb8aa4
pyk <x24870@gmail.com>
2022-04-24 16:31:34 +0800

    howSum

---
+++ DP/freeCodeCamp/dynamic_programming/howSum/js/main.js
@@ -0,0 +1,71 @@
+// m = target, n = len(arr)
+// time complexity: O(n^m)
+// space complexity: O(m)
+const howSum = (target, arr) => {
+    if (target === 0) return []
+    if (target < 0) return null
+
+    for (let i=0; i<arr.length; i++){
+
+        let reminder = target - arr[i]
+        let how = howSum(reminder, arr)
+
+        if (how !== null) {
+            how.push(arr[i])
+            return how
+        }
+    }
+
+    return null
+}
+
+// modern syntax
+// time complexity: O(n^m * m)
+// space complexity: O(m)
+const howSum2 = (target, arr) => {
+    if (target === 0) return []
+    if (target < 0) return null
+
+    for (let num of arr) {
+        let reminder = target - num;
+        let how = howSum2(reminder, arr);
+
+        if (how !== null){
+            how = [...how, num]; // recreate an array, considering the wrost case that the length of the array equals to target
+            return how
+        }
+    }
+
+    return null
+}
+
+// time complexity: O(n*m * m)
+// space complexity: O(m^2)
+const howSumMemo = (target, arr, memo={}) => {
+    if (memo[target] !== undefined) return memo[target]
+    if (target === 0) return []
+    if (target < 0) return null
+
+    for (let i=0; i<arr.length; i++){
+
+        let reminder = target - arr[i]
+        let how = howSumMemo(reminder, arr, memo)
+
+        if (how !== null) {
+            memo[target] = [...how, arr[i]]
+            return memo[target]
+        }
+    }
+
+    memo[target] = null
+    return memo[target]
+}
+
+console.log(howSum(7, [5,3,4,7])) // [4, 3]
+console.log(howSum(7, [2,4])) // null
+
+
+console.log(howSum2(7, [5,3,4,7])) // [4, 3]
+console.log(howSum2(7, [2,4])) // null
+
+console.log(howSumMemo(300, [7, 14])) // null
