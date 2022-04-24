faa045ab4252dcea7dc7ee5b97f2205efafb6092
pyk <x24870@gmail.com>
2022-04-24 21:58:30 +0800

    use map correctlly

--- DP/freeCodeCamp/dynamic_programming/howSum/main.go
+++ DP/freeCodeCamp/dynamic_programming/howSum/main.go
@@ -28,8 +28,8 @@ func howSum(target int, arr []int) []int {
 // time complexity: O(n*m * m)
 // space complexity: O:(m*m)
 func howSumMemo(target int, arr []int, memo map[int][]int) []int {
-	if memo[target] != nil {
-		return memo[target]
+	if val, ok := memo[target]; ok == true {
+		return val
 	}
 	if target == 0 {
 		return []int{}
@@ -56,8 +56,12 @@ func main() {
 	fmt.Println(howSum(8, []int{2, 3, 5}))    // [2, 2, 2, 2]
 	fmt.Println(howSum(7, []int{2, 4}))       // []
 
-	fmt.Println(howSum(7, []int{5, 3, 4, 7}))     // [3, 4]
-	fmt.Println(howSum(8, []int{2, 3, 5}))        // [2, 2, 2, 2]
-	fmt.Println(howSum(7, []int{2, 4}))           // []
-	fmt.Println(howSum(100, []int{10, 2, 4, 25})) // [10, 10...]
+	memo := make(map[int][]int)
+	fmt.Println(howSumMemo(7, []int{5, 3, 4, 7}, memo)) // [3, 4]
+	memo = make(map[int][]int)
+	fmt.Println(howSumMemo(7, []int{2, 4}, memo)) // []
+	memo = make(map[int][]int)
+	fmt.Println(howSumMemo(100, []int{10, 2, 4, 25}, memo)) // [10, 10...]
+	memo = make(map[int][]int)
+	fmt.Println(howSumMemo(300, []int{7, 14}, memo)) // []
 }
