89daeb5507048487e8ae86f3378d366d41002fa7
pyk <x24870@gmail.com>
2022-05-08 17:22:50 +0800

    gridTraveler tabulation

--- DP/freeCodeCamp/dynamic_programming/gridTraveler/js/main.js
+++ DP/freeCodeCamp/dynamic_programming/gridTraveler/js/main.js
@@ -9,7 +9,32 @@ const gridTravel = (m, n, memo={}) => {
     return memo[key]
 }
 
+const gridTravelTable = (m, n) => {
+    // init a 2D array
+    let table = Array(m+1)
+    .fill()
+    .map(() => Array(n+1).fill(0));
+
+    // init starter grid
+    table[1][1] = 1;
+
+    for (i=0; i<=m; i++) {
+        for (j=0; j<=n; j++) {
+            const cur = table[i][j]
+            if (j<n) table[i][j+1] += cur;
+            if (i<m) table[i+1][j] += cur;
+        }
+    }
+
+    return table[m][n]
+}
+
 console.log(gridTravel(2, 3)) //3
 console.log(gridTravel(3, 2)) //3
 console.log(gridTravel(3, 3)) //6
 console.log(gridTravel(18, 18)) // 2333606220
+
+console.log(gridTravelTable(2, 3)) //3
+console.log(gridTravelTable(3, 2)) //3
+console.log(gridTravelTable(3, 3)) //6
+console.log(gridTravelTable(18, 18)) // 2333606220
--- DP/freeCodeCamp/dynamic_programming/gridTraveler/main.go
+++ DP/freeCodeCamp/dynamic_programming/gridTraveler/main.go
@@ -42,6 +42,33 @@ func gridTravelWithMemo(m int, n int, memo map[string]int) int {
 	return memo[dimension]
 }
 
+func gridTravelTable(m int, n int) int {
+	// create a 2D array
+	table := make([][]int, m+1)
+	for i := range table {
+		table[i] = make([]int, n+1)
+	}
+
+	// init starter grid
+	table[1][1] = 1
+
+	// iterate the grid map
+	for i := 0; i <= m; i++ {
+		for j := 0; j <= n; j++ {
+			cur := table[i][j]
+			if j < n {
+				table[i][j+1] += cur // right grid
+			}
+			if i < m {
+				table[i+1][j] += cur // bottom grid
+			}
+
+		}
+	}
+
+	return table[m][n]
+}
+
 func main() {
 	fmt.Println(gridTravel(2, 3)) //3
 	fmt.Println(gridTravel(3, 2)) //3
@@ -53,4 +80,9 @@ func main() {
 	fmt.Println(gridTravelWithMemo(3, 2, memo))   //3
 	fmt.Println(gridTravelWithMemo(3, 3, memo))   //6
 	fmt.Println(gridTravelWithMemo(18, 18, memo)) // 2333606220 way faster
+
+	fmt.Println(gridTravelTable(2, 3))   //3
+	fmt.Println(gridTravelTable(3, 2))   //3
+	fmt.Println(gridTravelTable(3, 3))   //6
+	fmt.Println(gridTravelTable(18, 18)) // 2333606220 way faster
 }
