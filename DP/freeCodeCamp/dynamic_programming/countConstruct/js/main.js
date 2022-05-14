0e461357b8c57a0aa8227786b72e461bb979c52d
pyk <x24870@gmail.com>
2022-05-14 14:36:19 +0800

    countConstruct tabulation

--- DP/freeCodeCamp/dynamic_programming/countConstruct/js/main.js
+++ DP/freeCodeCamp/dynamic_programming/countConstruct/js/main.js
@@ -28,12 +28,46 @@ const countConstructMemo = (target, words, memo={}) => {
     return total
 }
 
+const countConstructTable = (target, words) => {
+    let table = Array(target.length + 1).fill(0);
+    table[0] = 1;
+
+    for (let i=0; i<table.length; i++) {
+        if (table[i] !== 0) {
+            for (let w of words) {
+                let start = i;
+                let end = start + w.length;
+                if (end < table.length && w === target.slice(start, end)) {
+                    table[end] += table[start];
+                }
+            }
+        }
+    }
+
+    return table[target.length];
+} 
 
 console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])) // 1
 console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) // 0
 console.log(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])) // 4
+console.log("---")
 
 console.log(countConstructMemo("eeeeeeeeeeeeeeeeeeeeeeeeeee", 
+[
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee"
+])) // 56058368
+console.log("---")
+
+console.log(countConstructTable("abcdef", ["ab", "abc", "cd", "def", "abcd"])) // 1
+console.log(countConstructTable("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) // 0
+console.log(countConstructTable("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])) // 4
+
+console.log(countConstructTable("eeeeeeeeeeeeeeeeeeeeeeeeeee", 
 [
     "e",
     "ee",
--- DP/freeCodeCamp/dynamic_programming/countConstruct/main.go
+++ DP/freeCodeCamp/dynamic_programming/countConstruct/main.go
@@ -46,18 +46,42 @@ func countConstructMemo(target string, words []string, memo map[string]int) int 
 	return total
 }
 
+// time complexity: O(m * n * m)
+// space complexity: O(m)
+func countConstructTable(target string, words []string) int {
+	table := make([]int, len(target)+1)
+
+	// considering target is a empty string, there is always 1 way to match construct it. Which means choose no word in the words list.
+	table[0] = 1
+
+	for i := 0; i < len(table); i++ {
+		if table[i] != 0 {
+			for _, w := range words {
+				start := i
+				end := i + len(w)
+				if end < len(table) && target[start:end] == w {
+					table[end] += table[start]
+				}
+			}
+		}
+	}
+
+	return table[len(target)]
+}
+
 func main() {
 	fmt.Println(countConstruct("abcdef", []string{"ab", "abc", "cd", "def", "abcd"}))                  // 1
 	fmt.Println(countConstruct("skateboard", []string{"bo", "rd", "ate", "t", "ska", "sk", "boar"}))   // 0
 	fmt.Println(countConstruct("enterapotentpot", []string{"a", "p", "ent", "enter", "ot", "o", "t"})) // 4
-	fmt.Println(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeee", []string{
-		"e",
-		"ee",
-		"eee",
-		"eeee",
-		"eeeee",
-		"eeeeee",
-	})) // 56058368
+	// fmt.Println(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeee", []string{
+	// 	"e",
+	// 	"ee",
+	// 	"eee",
+	// 	"eeee",
+	// 	"eeeee",
+	// 	"eeeeee",
+	// })) // 56058368
+	fmt.Println("---")
 
 	m := make(map[string]int)
 	fmt.Println(countConstructMemo("eeeeeeeeeeeeeeeeeeeeeeeeeee", []string{
@@ -68,4 +92,17 @@ func main() {
 		"eeeee",
 		"eeeeee",
 	}, m)) // 56058368
+	fmt.Println("---")
+
+	fmt.Println(countConstructTable("abcdef", []string{"ab", "abc", "cd", "def", "abcd"}))                  // 1
+	fmt.Println(countConstructTable("skateboard", []string{"bo", "rd", "ate", "t", "ska", "sk", "boar"}))   // 0
+	fmt.Println(countConstructTable("enterapotentpot", []string{"a", "p", "ent", "enter", "ot", "o", "t"})) // 4
+	fmt.Println(countConstructTable("eeeeeeeeeeeeeeeeeeeeeeeeeee", []string{
+		"e",
+		"ee",
+		"eee",
+		"eeee",
+		"eeeee",
+		"eeeeee",
+	})) // 56058368
 }
