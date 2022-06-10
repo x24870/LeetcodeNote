71c5ce6361b72bf3aea9f2778301366091ad9a08
x24870 <x24870@gmail.com>
2022-06-10 11:24:20 +0800

    23. Merge k Sorted Lists - go solution

--- List/23. Merge k Sorted Lists/main.go
+++ List/23. Merge k Sorted Lists/main.go
@@ -5,6 +5,34 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func deleteNode(node *ListNode) {
-	node.Val, node.Next = node.Next.Val, node.Next.Next
+func mergeKLists(lists []*ListNode) *ListNode {
+	var ret *ListNode
+	for _, list := range lists {
+		ret = merge(ret, list)
+	}
+	return ret
+}
+
+func merge(l1, l2 *ListNode) *ListNode {
+	dummyHead := &ListNode{}
+	cur := dummyHead
+	for l1 != nil && l2 != nil {
+		if l1.Val > l2.Val {
+			cur.Next = l2
+			l2 = l2.Next
+		} else {
+			cur.Next = l1
+			l1 = l1.Next
+		}
+		cur = cur.Next
+	}
+
+	if l1 != nil {
+		cur.Next = l1
+	}
+	if l2 != nil {
+		cur.Next = l2
+	}
+
+	return dummyHead.Next
 }
